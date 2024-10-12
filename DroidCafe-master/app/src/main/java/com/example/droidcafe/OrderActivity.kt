8a7457e5c6bfb14f9f2cb08933a77e4f67a6a392
supersyzen <84361552+supersyzenn@users.noreply.github.com>
2024-10-12 10:50:02 +0700

    Add files via upload

---
+++ DroidCafe-master/app/src/main/java/com/example/droidcafe/OrderActivity.kt
@@ -0,0 +1,48 @@
+package com.example.droidcafe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import android.widget.Toast
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+    }
+
+    fun onSpinnerClicked(){
+
+    }
+
+
+    fun onRadioButtonClicked(view: View) {
+        // Is the button now checked?
+        val checked = (view as RadioButton).isChecked
+        when (view.getId()) {
+            R.id.sameday -> if (checked) // Same day service
+                displayToast(getString(R.string.same_day_messenger_service))
+
+
+            R.id.nextday -> if (checked) // Next day delivery
+                displayToast(getString(R.string.next_day_ground_delivery))
+
+
+            R.id.pickup -> if (checked) // Pick up
+                displayToast(getString(R.string.pick_up))
+
+
+            else -> {}
+        }
+    }
+
+
+    private fun displayToast(message: String) {
+        Toast.makeText(
+            applicationContext, message,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+}
